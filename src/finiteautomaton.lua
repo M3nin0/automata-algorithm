2a036f94375dfc2f4264250f3154c84a203d0eb8
Felipe Menino <felipe.carlos@fatec.sp.gov.br>
2020-03-10 23:02:53 -0300

    organize repo to TerraME scripts

--- src/finiteautomaton.lua
+++ src/finiteautomaton.lua
@@ -37,3 +37,32 @@ function FiniteAutomatonDeterministic.changestate(self, alphabetSymbol)
         error("Symbol is not in Automaton Alphabet");
     end
 end
+
+-- Test code
+local states = {
+    'a', 'b', 'c'
+};
+
+local alphabet = {
+    '0', '1'
+};
+
+local transitionFunction = {
+    ['a'] = {
+        ['0'] = 'b',
+        ['1'] = 'a'
+    },
+    ['b'] = {
+        ['0'] = 'a',
+        ['1'] = 'b'
+    }
+}
+
+local initialState = 'a';
+local endState = {'b'};
+
+local automaton = FiniteAutomatonDeterministic:create(states, alphabet, 
+                                                        transitionFunction, initialState, endState);
+
+print(automaton.checkstring(automaton, '010'))
+print(automaton.checkstring(automaton, '0100'))
